ac9584e1a3fa1179bcd94deb5a59ebe2dd1f08ca
ryan-brown-cmyk <ryanbrown4877@gmail.com>
2026-09-09 17:01:09 -0400

    Expanded slint framework, and wavetables framework
    Still some issues with the slint, and the wavetables is not complete.

--- src/audio/wavetables.rs
+++ src/audio/wavetables.rs
@@ -6,21 +6,69 @@
 // determined by how many times you loop.
 
 pub enum WaveTableType {
-    SinWave,
-    TriangleWave,
-    SquareWave,
-    SawtoothWave,
-    PulseWave,
+    Sin,
+    Triangle,
+    Square,
+    Sawtooth,
+    Pulse,
+}
+
+pub struct WaveTableParams {
+    pub freq: f32,
+    pub phase: f32,
+    pub offset: f32,
+    pub wave_type: WaveTableType,
 }
 
 pub struct WaveTableContainer {
     pub wave: Vec<f64>,
-    freq: f32,
-    phase: f32,
-    offset: f32,
-    wave_type: WaveTableType,
+    pub params: WaveTableParams,
 }
 
-pub fn generate_wavetable(wave_type: WaveTableType) {
-    todo!();
+// For now, we will simply return an option. We will probably want to pass back if an error should
+// occur, however.
+pub fn generate_wavetable(wave_params: WaveTableParams) -> Option<WaveTableContainer> {
+    match wave_params.wave_type {
+        WaveTableType::Sin => {
+            return generate_sin(wave_params);
+        }
+        WaveTableType::Triangle => {
+            return generate_triangle(wave_params);
+        }
+        WaveTableType::Square => {
+            return generate_square(wave_params);
+        }
+        WaveTableType::Sawtooth => {
+            return generate_sawtooth(wave_params);
+        }
+        WaveTableType::Pulse => {
+            return generate_pulse(wave_params);
+        }
+    }
+}
+
+fn generate_sin(wave_params: WaveTableParams) -> Option<WaveTableContainer> {
+    let wave = new(WaveTableContainer);
+
+    return wave;
+}
+fn generate_triangle(wave_params: WaveTableParams) -> Option<WaveTableContainer> {
+    let wave = new(WaveTableContainer);
+
+    return wave;
+}
+fn generate_square(wave_params: WaveTableParams) -> Option<WaveTableContainer> {
+    let wave = new(WaveTableContainer);
+
+    return wave;
+}
+fn generate_sawtooth(wave_params: WaveTableParams) -> Option<WaveTableContainer> {
+    let wave = new(WaveTableContainer);
+
+    return wave;
+}
+fn generate_pulse(wave_params: WaveTableParams) -> Option<WaveTableContainer> {
+    let wave = new(WaveTableContainer);
+
+    return wave;
 }
